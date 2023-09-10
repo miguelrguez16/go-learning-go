449532a966f409c9eb6524808902a4580dbe2bbe
Miguel Rodríguez González <uo263494@uniovi.es>
2023-09-10 20:12:37 +0200

    feat: sync and mutex

--- go-routines/main.go
+++ go-routines/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go-routines/data"
 	"log"
 	"math/rand"
 	"sync"
@@ -13,22 +14,26 @@ import (
 func main() {
 	log.Println("Application start")
 	tStart := time.Now()
+
 	var wg = &sync.WaitGroup{}
+	var mutex = &sync.Mutex{}
 
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go show(i, wg)
+		go readBook(i, wg, mutex)
 	}
 
 	wg.Wait()
-
+	data.ToString()
 	log.Printf("Total time %v ms", time.Since(tStart).Milliseconds())
+
 	log.Println("Application End")
 }
 
-func show(id int, wg *sync.WaitGroup) {
+func readBook(id int, wg *sync.WaitGroup, mutex *sync.Mutex) {
+	data.FinishBook(id, mutex)
 	delay := rand.Intn(500)
-	fmt.Printf("GOROUTINE #%d with %dms\n", id, delay)
+	fmt.Printf("GOROUTINE read book #%d with %dms\n", id, delay)
 	time.Sleep(time.Millisecond * time.Duration(delay))
 	wg.Done()
 }
---
+++ utils/enableCGO.bat
@@ -0,0 +1,4 @@
+@echo off
+SET CGO_ENABLED=1
+
+set | findstr  CGO_ENABLED
